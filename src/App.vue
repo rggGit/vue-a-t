a92da17f16becd0775564e334e188f0876fad997
Rafael González García <rafael.dsgn@gmail.com>
2020-02-06 22:43:21 +0100

    [js + pug + sass] Vue install
    VueJS install & first path

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  main#app
+    header
+      h1#main-name VueJS A-T
+      p VueJS Accessibility Training
+
+    section
+      h2 Section
+
+      router-view
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass">
+  @import '@/styles/app'
+</style>
